07f394452453a77ad9121423f1348635b6330f8e
Ravindra Naik <naik899@users.noreply.github.com>
2017-12-27 11:12:53 +0530

    Helper changes

---
+++ ReadFromImage/Helpers/ImageParser.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tesseract;
+
+namespace ReadFromImage.Helpers
+{
+    public class ImageParser
+    {
+        internal static string GetTextFromImage(Bitmap imgsource)
+        {
+            try
+            {
+                var ocrtext = string.Empty;
+                var tessdataPath = string.Empty; // this should contain the tess data in the folder tessdata
+                var engine = new TesseractEngine(tessdataPath, "eng");
+                using (var img = PixConverter.ToPix(imgsource))
+                {
+                    using (var page = engine.Process(img))
+                    {
+                        ocrtext = page.GetText();
+                    }
+                }
+
+                return ocrtext;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception at GetTextFromImage: " + ex.ToString());
+
+                return ex.ToString();
+            }
+        }
+    }
+}
---
+++ ReadFromImage/Program.cs
@@ -0,0 +1,21 @@
+﻿using ReadFromImage.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadFromImage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = string.Empty;
+            var bitMap = new Bitmap(filePath);
+            var finalString = ImageParser.GetTextFromImage(bitMap);
+            Console.WriteLine(finalString);
+        }
+    }
+}
